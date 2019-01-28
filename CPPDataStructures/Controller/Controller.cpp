112ebea25401ee0415f7a20f9b6af890a41b676f
HunterKnott <hunterknott00@gmail.com>
2019-01-28 12:45:55 -0700

    Initial commit

---
+++ CPPDataStructures/Controller/Controller.cpp
@@ -0,0 +1,14 @@
+//
+//  Controller.cpp
+//  CPPDataStructures
+//
+//  Created by Knott, Hunter on 1/28/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+void Controller :: start()
+{
+    cout << "Hello not world" << endl;
+}
---
+++ CPPDataStructures/Controller/Runner.cpp
@@ -0,0 +1,15 @@
+//
+//  Runner.cpp
+//  CPPDataStructures
+//
+//  Created by Knott, Hunter on 1/28/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+int main()
+{
+    Controller app;
+    app.start();
+}
