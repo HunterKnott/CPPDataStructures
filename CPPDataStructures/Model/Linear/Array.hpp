e148c3a2613fcc2dffecceb76d4134468665d21f
HunterKnott <hunterknott00@gmail.com>
2019-01-30 13:09:00 -0700

    Added the public section to the Array class

--- CPPDataStructures/Model/Linear/Array.hpp
+++ CPPDataStructures/Model/Linear/Array.hpp
@@ -20,6 +20,24 @@ class Array
 private:
     Type * internalArray;//Example of a pointer
     int size;
+public:
+    //Constructor
+    Array<Type>(int size);
+    
+    //Copy Constructor
+    Array<Type>(const Array<Type> & toCopy);
+    //Destructor
+    ~Array<Type>();
+    
+    //Operators
+    Array<Type> & operator = (const Array<Type> & toReplace);
+    Type& operator [] (int index);
+    Type operator [] (int index) const;
+    
+    //Methods
+    int getSize() const;
+    Type getFromIndex(int index);
+    void setAtIndex(int index, Type data);
 }
 
 #endif /* Array_hpp */
