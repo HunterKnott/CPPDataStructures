b3b8873f7331fb20dc61648fea67316b46ad6e65
HunterKnott <hunterknott00@gmail.com>
2019-02-01 13:08:45 -0700

    Created the Graph class inside the new NonLinear folder

---
+++ CPPDataStructures/Model/NonLinear/Graph.hpp
@@ -0,0 +1,58 @@
+//
+//  Graph.hpp
+//  CPPDataStructures
+//
+//  Created by Knott, Hunter on 2/1/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#ifndef Graph_hpp
+#define Graph_hpp
+
+#include <set>
+#include <queue>
+#include <assert.h>
+
+using namespace std;
+template <class Type>
+class Graph
+{
+private:
+    static const int MAXIMUM = 50;
+    bool adjacencyMatrix [MAXIMUM][MAXIMUM];
+    int weightCostMatrix [MAXIMUM][MAXIMUM];
+    Type graphData[MAXIMUM];
+    int vertexCount;
+    void depthFirstTraversal(Graph<Type> & graph, int vertex, bool markedVerticies[]);
+public:
+    Graph();
+    //Graph operations
+    
+    void addVertex(const Type& value);
+    
+    //Connect verticies
+    void addEdge(int source, int target);
+    void addEdgeUndirected(int source, int target);
+    void addEdgeCost(int source, int target, int cost);
+    //Disconnect verticies
+    void removeEdge(int source, int target);
+    void removeEdgeUndirected(int source, int target);
+    void removeEdgeCost(int source, int target);
+    
+    //Accessors
+    Type& operator [] (int vertex);
+    Type operator [] (int vertex) const;
+    int size() const;
+    
+    //Check connections
+    bool hasUndirectedConnection(int source, int target) const;
+    bool areConnected(int source, int target) const;
+    std::set<int> neighbors(int vertex) const;
+    
+    //Traversals
+    void depthFirstTraversal(Graph<Type> & graph, int vertex);
+    void breadthFirstTraversal(Graph<Type> & graph, int vertex);
+    int costTraversal(Graph<Type> & graph, int vertex);
+}
+
+#endif /* Graph_h */
