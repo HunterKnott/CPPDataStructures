96899e9718df6a037525d8fdb161418d92d0973c
HunterKnott <hunterknott00@gmail.com>
2019-02-01 12:06:06 -0700

    Created List class in Nodes folder

---
+++ CPPDataStructures/Model/Nodes/List.hpp
@@ -0,0 +1,15 @@
+//
+//  List.hpp
+//  CPPDataStructures
+//
+//  Created by Knott, Hunter on 2/1/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#ifndef List_hpp
+#define List_hpp
+
+#include <assert.h>
+#include "../../Nodes/LinearNode.hpp"
+
+#endif /* List_hpp */
