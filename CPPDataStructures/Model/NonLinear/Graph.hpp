4fae92de148acdb01ae477e9aa67224137ecf31b
HunterKnott <hunterknott00@gmail.com>
2019-02-04 13:23:33 -0700

    Finished the Traversals section of the Graph.hpp file

--- CPPDataStructures/Model/NonLinear/Graph.hpp
+++ CPPDataStructures/Model/NonLinear/Graph.hpp
@@ -203,6 +203,17 @@ void Graph<Type> :: depthFirstTraversal(Graph<Type> & currentGraph, int vertex, 
 {
     std::set<int> connections = currentGraph.neighbors(vertex);
     std::set<int>::iterator setIterator;
+    
+    visited[vertex] = true;
+    cout << currentGraph[vertex] << ", " << endl;
+    
+    for(setIterator = connections.begin(); setIterator != connections.end(); setIterator++)
+    {
+        if(!visited[*setIterator])
+        {
+            depthFirstTraversal(currentGraph, *setIterator, visited);
+        }
+    }
 }
 
 #endif /* Graph_h */
