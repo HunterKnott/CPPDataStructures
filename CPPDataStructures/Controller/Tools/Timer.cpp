035012d5aedaa5f346c23e920b075c4c0749921d
HunterKnott <hunterknott00@gmail.com>
2019-02-05 12:07:38 -0700

    Created hpp and hpp files named Timer

---
+++ CPPDataStructures/Controller/Tools/Timer.cpp
@@ -0,0 +1,9 @@
+//
+//  Timer.cpp
+//  CPPDataStructures
+//
+//  Created by Knott, Hunter on 2/5/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#include "Timer.hpp"
---
+++ CPPDataStructures/Controller/Tools/Timer.hpp
@@ -0,0 +1,22 @@
+//
+//  Timer.hpp
+//  CPPDataStructures
+//
+//  Created by Knott, Hunter on 2/5/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#ifndef Timer_hpp
+#define Timer_hpp
+
+#include <stdio.h>
+#include <time.h>
+#include <assert.h>
+#include <iostream>
+
+class Timer
+{
+    
+};
+
+#endif /* Timer_hpp */
