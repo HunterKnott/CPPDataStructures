3646f5cd5bde1eadfc46a41141dc672f8166ed68
HunterKnott <hunterknott00@gmail.com>
2019-02-05 12:26:35 -0700

    Created FileCController.cpp and .hpp files

---
+++ CPPDataStructures/Controller/FileController.cpp
@@ -0,0 +1,9 @@
+//
+//  FileController.cpp
+//  CPPDataStructures
+//
+//  Created by Knott, Hunter on 2/5/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#include "FileController.hpp"
---
+++ CPPDataStructures/Controller/FileController.hpp
@@ -0,0 +1,22 @@
+//
+//  FileController.hpp
+//  CPPDataStructures
+//
+//  Created by Knott, Hunter on 2/5/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#ifndef FileController_hpp
+#define FileController_hpp
+
+#include "../Resources/CrimeData.hpp"
+#include "../Resources/Music.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+#include <stdio.h>
+
+#endif /* FileController_hpp */
