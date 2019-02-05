d613b4518183e04c0e9797791c9b76555d232e66
HunterKnott <hunterknott00@gmail.com>
2019-02-05 12:50:50 -0700

    Created read methods for CrimeData and Music

--- CPPDataStructures/Controller/FileController.cpp
+++ CPPDataStructures/Controller/FileController.cpp
@@ -7,3 +7,77 @@
 //
 
 #include "FileController.hpp"
+
+vector<CrimeData> FileController :: readCrimeDataToVector(string filename)
+{
+    std :: vector<CrimeData> crimeVector;
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(filename);
+    
+    //If the file exists at that path
+    if(dataFile.is_open())
+    {
+        //Keep reading until you are at the end of the file
+        while(!dataFile.eof())
+        {
+            //Grab each line from the CSV separated by the carriage return character
+            getLine(dataFile, currentCSVLine, '\r');
+            //Exclude header row
+            if(rowCount != 0)
+            {
+                //Create a CrimeData instance from the line. Exlude a blank line (usually if opened separately)
+                if(currentCSVLine.length() != 0)
+                {
+                    CrimeData row(currentCSVLine);
+                    crimeVector.push_back(row);
+                }
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    return crimeVector;
+}
+
+vector<Music> FileController :: musicDataToVector(string filename)
+{
+    vector<Music> musicVector;
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(filename);
+    
+    //If the file exists at that path
+    if(dataFile.is_open())
+    {
+        //Keep reading until you are at the end of the file
+        while(!dataFile.eof())
+        {
+            //Grab each line from the CSV separated by the carriage return character
+            getLine(dataFile, currentCSVLine, '\r');
+            //Exclude header row
+            if(rowCount != 0)
+            {
+                //Create a Music instance from the line. Exlude a blank line (usually if opened separately)
+                if(currentCSVLine.length() != 0)
+                {
+                    Music row(currentCSVLine);
+                    musicVector.push_back(row);
+                }
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    return musicVector;
+}
