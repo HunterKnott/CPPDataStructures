f5d45379032ee9fc36093a802ec90f3fc064b3aa
HunterKnott <hunterknott00@gmail.com>
2019-02-07 13:03:19 -0700

    Created LinkedList class in the Linear folder

---
+++ CPPDataStructures/Model/Linear/LinkedList.hpp
@@ -0,0 +1,17 @@
+//
+//  LinkedList.hpp
+//  CPPDataStructures
+//
+//  Created by Knott, Hunter on 2/7/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#include "List.hpp"
+
+using namespace std; //Used for keyword access. Use this to make sure your pointers are pointing to the right things
+
+#ifndef LinkedList_hpp
+#define LinkedList_hpp
+
+
+#endif /* LinkedList_h */
