e13b7870ff58df17c50e80312539f1ff9d422f05
HunterKnott <hunterknott00@gmail.com>
2019-02-07 13:30:20 -0700

    Made the addAtIndex method in LinkedList

--- CPPDataStructures/Model/Linear/LinkedList.hpp
+++ CPPDataStructures/Model/Linear/LinkedList.hpp
@@ -74,5 +74,36 @@ void LinkedList<Type> :: add(Type item)//add(Type) method must know the size of 
     this-> size += 1;
 }
 
+void LinkedList<Type> :: addAtIndex(int index, Type item)
+{
+    assert(index >= 0 && index <= this->size);
+    if(index == this->size)//Checks to see if index is the size of the current list
+    {
+        add(item);
+    }
+    else//Handles moving front pointer if index and list are different sizes
+    {
+        LinearNode<Type> * toBeAdded = new LinearNode<Type>(item);
+        if(index == 0)
+        {
+            toBeAdded->setNextNode(front);
+            front = toBeAdded;
+        }
+        else
+        {
+            LinearNode<Type> * previous = nullptr;
+            LinearNode<Type> * current = front;
+            for(int position = 0; position < index; position++)
+            {
+                previous = current;
+                current = current->getNextNode();
+            }
+            previous->setNextNode(toBeAdded);
+            toBeAdded->setNextNode(current);
+        }
+        this->size++;
+    }
+}
+
 
 #endif /* LinkedList_h */
