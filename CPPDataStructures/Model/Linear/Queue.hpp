6e180ee7b419f121189b89cd3de40c73749a5121
HunterKnott <hunterknott00@gmail.com>
2019-02-11 13:07:09 -0700

    Created Stack.hpp and Queue.hpp in the Linear folder

---
+++ CPPDataStructures/Model/Linear/Queue.hpp
@@ -0,0 +1,13 @@
+//
+//  Queue.hpp
+//  CPPDataStructures
+//
+//  Created by Knott, Hunter on 2/11/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#ifndef Queue_h
+#define Queue_h
+
+
+#endif /* Queue_h */
