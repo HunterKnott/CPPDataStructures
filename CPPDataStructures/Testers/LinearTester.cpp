e10d63d2b26b8aacb22545cde03b872c607d3f64
HunterKnott <hunterknott00@gmail.com>
2019-02-13 13:04:28 -0700

    Made LinearTester.cpp and LinearTester.hpp in the new Testers folder

---
+++ CPPDataStructures/Testers/LinearTester.cpp
@@ -0,0 +1,9 @@
+//
+//  LinearTester.cpp
+//  CPPDataStructures
+//
+//  Created by Knott, Hunter on 2/13/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#include "LinearTester.hpp"
