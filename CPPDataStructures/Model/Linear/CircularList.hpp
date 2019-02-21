9f0b98db09e8a787a2a35d677a1633508871acd4
HunterKnott <hunterknott00@gmail.com>
2019-02-21 12:31:27 -0700

    Made the add method in CircularList.hpp

--- CPPDataStructures/Model/Linear/CircularList.hpp
+++ CPPDataStructures/Model/Linear/CircularList.hpp
@@ -76,4 +76,24 @@ DoubleNode<Type> * CircularList<Type> :: findNode(int index)
     return nodeToFind;
 }
 
+template <class Type>
+void CircularList<Type> :: add(Type item)
+{
+    DoubleNode<Type> * addedNode;
+    if(this->size == 0)
+    {
+        addedNode = new DoubleNode<Type>(item);
+        this->front = addedNode;
+    }
+    else
+    {
+        addedNode = new DoubleNode<Type>(item, this->end, this->front);
+    }
+    
+    this->end->setNext(addedNode);
+    this->front->setPrevious(addedNode);
+    this->end = addedNode;
+    this->size++;
+}
+
 #endif /* CircularList_hpp */
