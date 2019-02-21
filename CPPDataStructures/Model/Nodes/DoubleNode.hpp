efd02a5e76a4d20ed5d5bebfeb2f654158560b45
HunterKnott <hunterknott00@gmail.com>
2019-02-21 11:37:46 -0700

    Made the new DoubleNode.hpp file in Nodes folder

---
+++ CPPDataStructures/Model/Nodes/DoubleNode.hpp
@@ -0,0 +1,14 @@
+//
+//  DoubleNode.hpp
+//  CPPDataStructures
+//
+//  Created by Knott, Hunter on 2/21/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#ifndef DoubleNode_hpp
+#define DoubleNode_hpp
+
+#include "Node.hpp"
+
+#endif /* DoubleNode_hpp */
