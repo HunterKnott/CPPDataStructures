c960c3f5b0dd1b907d3b6c53e6a0172b76499e17
HunterKnott <hunterknott00@gmail.com>
2019-02-21 12:14:32 -0700

    Made the CircularList.hpp file in the Linear folder

---
+++ CPPDataStructures/Model/Linear/CircularList.hpp
@@ -0,0 +1,14 @@
+//
+//  CircularList.hpp
+//  CPPDataStructures
+//
+//  Created by Knott, Hunter on 2/21/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#ifndef CircularList_hpp
+#define CircularList_hpp
+
+#include "../Nodes/DoubleNode.hpp"
+
+#endif /* CircularList_hpp */
