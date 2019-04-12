26f2628ac2eea01416e0bf298d5737db45b8d020
Hunter <hunterknott00@gmail.com>
2019-04-12 12:47:57 -0600

    Made abstract Tree.hpp file

---
+++ CPPDataStructures/Model/NonLinear/Tree.hpp
@@ -0,0 +1,36 @@
+//
+//  Tree.hpp
+//  BackupCPPDataStructures
+//
+//  Created by Knott, Hunter on 4/12/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#ifndef Tree_hpp
+#define Tree_hpp
+#include "../Nodes/BinaryTreeNode.hpp"
+
+template<class Type>
+class Tree
+{
+protected:
+    BinaryTreeNode<Type> * root;
+public:
+    /*Informational Methods*/
+    virtual int getSize() = 0;
+    virtual int getHeight() = 0;
+    virtual bool isComplete() = 0;
+    virtual bool isBalanced() = 0;
+    
+    /*Data Methods*/
+    virtual void insert(Type data) = 0;
+    virtual bool contains(Type data) = 0;
+    virtual void remove(Type data) = 0;
+    
+    /*Traversals*/
+    virtual void inOrderTraversal() = 0;
+    virtual void preOrderTraversal() = 0;
+    virtual void postOrderTraversal() = 0;
+};
+
+#endif /* Tree_hpp */
