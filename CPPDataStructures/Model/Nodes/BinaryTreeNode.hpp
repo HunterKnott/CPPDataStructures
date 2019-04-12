dc96ad5306c9c60316d328897e8246c09e25fcc1
Hunter <hunterknott00@gmail.com>
2019-04-12 12:20:04 -0600

    Made the new BinaryTreeNode.hpp file

---
+++ CPPDataStructures/Model/Nodes/BinaryTreeNode.hpp
@@ -0,0 +1,13 @@
+//
+//  BinaryTreeNode.hpp
+//  BackupCPPDataStructures
+//
+//  Created by Knott, Hunter on 4/12/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#ifndef BinaryTreeNode_hpp
+#define BinaryTreeNode_hpp
+
+
+#endif /* BinaryTreeNode_hpp */
