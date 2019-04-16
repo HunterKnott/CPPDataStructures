e5da66a243ca24cb08001c3ee87653f778b1cadc
Hunter <hunterknott00@gmail.com>
2019-04-16 12:15:27 -0600

    Made data operation method stubs in BinarySearchTree.hpp

--- CPPDataStructures/Model/NonLinear/BinarySearchTree.hpp
+++ CPPDataStructures/Model/NonLinear/BinarySearchTree.hpp
@@ -104,4 +104,23 @@ void BinarySearchTree<Type> :: postOrderTraversal()
     
 }
 
+//MARK: Data Operation
+template<class Type>
+void BinarySearchTree<Type> :: insert(Type item)
+{
+    
+}
+
+template<class Type>
+bool BinarySearchTree<Type> :: contains(Type value)
+{
+    return false;
+}
+
+template<class Type>
+void BinarySearchTree<Type> :: remove(Type item)
+{
+    
+}
+
 #endif /* BinarySearchTree_hpp */
