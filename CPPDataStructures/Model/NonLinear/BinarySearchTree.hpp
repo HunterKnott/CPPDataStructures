3e8a34b11a31cd8c089030981d843fa2dfcc205a
Hunter <hunterknott00@gmail.com>
2019-04-16 12:30:12 -0600

    Implemented insert method in BinarySearchTree.hpp

--- CPPDataStructures/Model/NonLinear/BinarySearchTree.hpp
+++ CPPDataStructures/Model/NonLinear/BinarySearchTree.hpp
@@ -106,9 +106,47 @@ void BinarySearchTree<Type> :: postOrderTraversal()
 
 //MARK: Data Operation
 template<class Type>
-void BinarySearchTree<Type> :: insert(Type item)
+void BinarySearchTree<Type> :: insert(Type itemToInsert)
 {
+    BinaryTreeNode<Type> * insertMe = new BinaryTreeNode<Type>(itemToInsert);
+    BinaryTreeNode<Type> * previous = nullptr;
+    BinaryTreeNode<Type> * current = this->root;
     
+    if(current == nullptr)
+    {
+        this->root = insertMe;
+    }
+    else
+    {
+        while(current != nullptr)
+        {
+            previous = current;
+            if(itemToInsert < current->getData())
+            {
+                current = current->getLeftChild();
+            }
+            else if(itemToInsert > current->getData())
+            {
+                current = current->getRightChild();
+            }
+            else
+            {
+                cerr << "Item exists already - Exiting insert" << endl;
+                delete insertMe;
+                return;
+            }
+        }
+        
+        if(previous->getData() > itemToInsert)
+        {
+            previous->setLeftChild(insertMe);
+        }
+        else
+        {
+            previous->setRightChild(insertMe);
+        }
+        insertMe->setRootNode(previous);
+    }
 }
 
 template<class Type>
