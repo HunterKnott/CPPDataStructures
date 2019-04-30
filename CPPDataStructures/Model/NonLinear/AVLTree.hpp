779cfc3a7cad8ed273aef6422503cb296062b49b
Hunter <hunterknott00@gmail.com>
2019-04-30 11:51:49 -0600

    Made new AVLTree class with class definition

---
+++ CPPDataStructures/Model/NonLinear/AVLTree.hpp
@@ -0,0 +1,37 @@
+//
+//  AVLTree.hpp
+//  BackupCPPDataStructures
+//
+//  Created by Knott, Hunter on 4/30/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+#ifndef AVLTree_hpp
+#define AVLTree_hpp
+
+#include "BinarySearchTree.hpp"
+
+template<class Type>
+class AVLTree : public BinarySearchTree<Type>
+{
+private:
+    BinaryTreeNode<Type> * leftRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * leftRightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightLeftRotation(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * balanceSubTree(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * insertNode(BinaryTreeNode<Type> * parent, Type value);
+    BinaryTreeNode<Type> * removeNode(BinaryTreeNode<Type> * parent, Type value);
+    
+    int heightDifference(BinaryTreeNode<Type> * parent);
+    
+public:
+    AVLTree();
+    
+    void insert(Type itemToInsert);
+    void remove(Type value);
+};
+
+#endif /* AVLTree_hpp */
